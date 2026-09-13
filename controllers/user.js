f7285dc7fcb6bf255b6dc6729c9f676cad45150c
Shubham Pawar <shubhamm27p@gmail.com>
2026-09-14 00:04:47 +0530

    Update views, styling, user controllers and clean up tmp files

--- controllers/user.js
+++ controllers/user.js
@@ -25,7 +25,7 @@ module.exports.signupUser = async (req, res, next) => {
             if (err) {
                 return next(err);
             }
-            req.flash("success", "Welcome to Wanderlust!");
+            req.flash("success", "Welcome to Wayora!");
             res.redirect(redirectUrl);
         });       
     } catch (e) {
